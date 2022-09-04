baf738dac868a193390851e247599275df9cdd35
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-04 21:50:14 +0530

    [Prajakta] Add: Add a New Contact

---
+++ AddressBook/Addressbook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Addressbook
+    {
+        List<Contact> contacts=new List<Contact>();
+        public void AddContact()
+        {
+            Contact contact = new Contact();
+            Console.WriteLine("---------------------------------\n");
+            Console.WriteLine("Enter FirstName: ");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName: ");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address: ");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City: ");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State: ");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip: ");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNo: ");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email: ");
+            contact.Email = Console.ReadLine();
+            Console.WriteLine("---------------------------------\n");
+            Console.WriteLine("The New Contact Added is: \n");
+            Console.WriteLine("FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zip: " + contact.Zip + "\n" + "PhoneNumber: " + contact.PhoneNumber + "\n" + "Email: " + contact.Email);
+            Console.ReadKey();
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,18 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Contact contact = new Contact()
+            Console.WriteLine("1.Add\n ");
+            Console.WriteLine("Please Enter Your Choice : ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
             {
-                FirstName = "Prajakta",
-                LastName = "Jagtap",
-                Address = "M.J.Road",
-                City = "Pune",
-                State = "Maharastra",
-                Zip = 412210,
-                PhoneNumber = "8329011606",
-                Email = "xyz@gmail.com"
-            };
-            Console.WriteLine("FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zip: " + contact.Zip + "\n" + "PhoneNumber: " + contact.PhoneNumber + "\n" + "Email: " + contact.Email);
+                case 1:
+                    Addressbook addressbook = new Addressbook();
+                    addressbook.AddContact();
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
