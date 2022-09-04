b54c8f4dd313add02ba338d20190b40eba1a85b3
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-09-04 20:35:32 +0530

    [Prajakta] Add: To Create Contact in Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book Program");
+            Contact contact = new Contact()
+            {
+                FirstName = "Prajakta",
+                LastName = "Jagtap",
+                Address = "M.J.Road",
+                City = "Pune",
+                State = "Maharastra",
+                Zip = 412210,
+                PhoneNumber = "8329011606",
+                Email = "xyz@gmail.com"
+            };
+            Console.WriteLine("FirstName: " + contact.FirstName + "\n" + "LastName: " + contact.LastName + "\n" + "Address: " + contact.Address + "\n" + "City: " + contact.City + "\n" + "State: " + contact.State + "\n" + "Zip: " + contact.Zip + "\n" + "PhoneNumber: " + contact.PhoneNumber + "\n" + "Email: " + contact.Email);
         }
     }
 }
